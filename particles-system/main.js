18edf1c9d901d009fd1f24b8e37547772c5636ef
siwachs <ssiwach.8888@gmail.com>
2023-11-20 13:48:32 +0530

    Particle effect

---
+++ particles-system/main.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById("particle-effect");
+const ctx = canvas.getContext("2d");
+const particlesArray = [];
+
+function setCanvasSize() {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+}
+
+setCanvasSize();
+window.addEventListener("resize", setCanvasSize);
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+canvas.addEventListener("click", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+(function init() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+})();
+console.log(particlesArray);
+
+function handleParticles() {
+  for (let i = 0; i < 100; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+animate();
